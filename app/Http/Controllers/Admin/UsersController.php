660148c79b12cd6e120df6d25b0812e28e0acee9
ahmeterkan <ahmete48@hotmail.com>
2022-07-25 19:03:39 +0300

    düzenlenecek kullanıcının bilgileri getirildi

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -13,4 +13,13 @@ class UsersController extends Controller
         $users= User::all();
         return view('admin.users', ['users' => $users]);
     }
+    public function get(Request $request)
+    {
+        $user = User::where("id", $request->id)->first();
+        return response()->json([
+            'status' => 2,
+            'user' => $user,
+            'response' => "Kullanıcı Getirildi!"
+        ], 200);
+    }
 }
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -35,7 +35,7 @@
                                             </button>
                                             <div class="dropdown-menu animated--fade-in"
                                                 aria-labelledby="dropdownMenuButton">
-                                                <a class="dropdown-item editReservation" href="#"
+                                                <a class="dropdown-item editUser" href="#"
                                                     data-id="{{ $res->id }}">
                                                     Düzenle </a>
                                                 <a class="dropdown-item showqr" href="#"
@@ -59,26 +59,6 @@
 
         </div>
     </div>
-    <div class="modal" tabindex="-1" role="dialog" id="qrModal">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Rezervasyon QR Kodu</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body qr" style="text-align: center">
-                    <div class=""></div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary printQr">Yazdır</button>
-                    <button type="button" class="btn btn-info sendMail">E-mail Gönder</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="modal" tabindex="-1" role="dialog" id="editModal">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -89,43 +69,22 @@
                     </button>
                 </div>
                 <div class="modal-body qr" style="text-align: center">
-                    <form class="user reservationEdit">
+                    <form class="user userEdit">
                         @csrf
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="text" class="form-control form-control-user" id="FirstName" name="FirstName"
-                                    placeholder="Ad">
+                                <input type="text" class="form-control form-control-user" id="name" name="name"
+                                    placeholder="Ad Soyad" autocomplete="off">
                             </div>
                             <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-user" id="LastName" name="LastName"
-                                    placeholder="Soyad">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="text" class="form-control form-control-user" id="Amount" name="Amount"
-                                    placeholder="Tutar">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-user" id="Pax" name="Pax"
-                                    placeholder="Kişi Sayısı">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="text" class="form-control form-control-user" id="TableNumber"
-                                    name="TableNumber" placeholder="Masa Numarası">
-                            </div>
-                            <div class="col-sm-6">
-                                <input class="form-control form-control-user" id="CheckInDateModal" type="text"
-                                    name="CheckInDate" placeholder="Tarih" />
+                                <input type="text" class="form-control form-control-user" id="email" name="email"
+                                    placeholder="E-mail" autocomplete="off">
                             </div>
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-12 mb-3 mb-sm-0">
-                                <label for="ReservationNote">Rezervasyon Notu:</label>
-                                <textarea type="textarea" class="form-control " id="ReservationNote" name="ReservationNote" rows="5">
-                                </textarea>
+                                <input type="password" class="form-control form-control-user" id="Amount" name="password"
+                                    placeholder="Şifre" autocomplete="off">
                             </div>
                         </div>
                         <input type="hidden" name="id">
@@ -141,160 +100,29 @@
 @endsection
 
 @section('script')
-    {{-- // daterangepicker start --}}
-    {{-- <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script> --}}
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    {{-- // daterangepicker end --}}
-    <script src="{{ url('js/printThis.js') }}"></script>
+<script>
 
-    <script>
-        // daterangepicker start
-        $('input[id="CheckInDate"]').daterangepicker({
-            "locale": {
-                "format": "DD/MM/YYYY",
-                "separator": " - ",
-                "applyLabel": "Uygula",
-                "cancelLabel": "İptal",
-                "fromLabel": "From",
-                "toLabel": "To",
-                "customRangeLabel": "Custom",
-                "weekLabel": "H",
-                "daysOfWeek": [
-                    "Pa",
-                    "Pt",
-                    "Sa",
-                    "Ça",
-                    "Pe",
-                    "Cu",
-                    "Ct"
-                ],
-                "monthNames": [
-                    "Ocak",
-                    "Şubat",
-                    "Mart",
-                    "Nisan",
-                    "Mayıs",
-                    "Haziran",
-                    "Temmuz",
-                    "Ağustos",
-                    "Eylül",
-                    "Ekim",
-                    "Kasım",
-                    "Aralık"
-                ],
-                "firstDay": 1
-            },
-            ranges: {
-                'Bugün': [moment(), moment()],
-                'Dün': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Son 7 Gün': [moment().subtract(6, 'days'), moment()],
-                'Son 30 Gün': [moment().subtract(29, 'days'), moment()],
-                'Bu Ay': [moment().startOf('month'), moment().endOf('month')],
-                'Geçen Ay': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
-                    'month').endOf('month')]
-            }
-        }, function(start, end, label) {
-            console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format(
-                'YYYY-MM-DD') + ' (predefined range: ' + label + ')');
-        });
-
-        $('input[id="CheckInDateModal"]').daterangepicker({
-            "singleDatePicker": true,
-            "locale": {
-                "format": "DD/MM/YYYY",
-                "separator": " - ",
-                "applyLabel": "Uygula",
-                "cancelLabel": "İptal",
-                "fromLabel": "From",
-                "toLabel": "To",
-                "customRangeLabel": "Custom",
-                "weekLabel": "H",
-                "daysOfWeek": [
-                    "Pa",
-                    "Pt",
-                    "Sa",
-                    "Ça",
-                    "Pe",
-                    "Cu",
-                    "Ct"
-                ],
-                "monthNames": [
-                    "Ocak",
-                    "Şubat",
-                    "Mart",
-                    "Nisan",
-                    "Mayıs",
-                    "Haziran",
-                    "Temmuz",
-                    "Ağustos",
-                    "Eylül",
-                    "Ekim",
-                    "Kasım",
-                    "Aralık"
-                ],
-                "firstDay": 1
-            },
-            "autoApply": true,
-        }, function(start, end, label) {
-            console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format(
-                'YYYY-MM-DD') + ' (predefined range: ' + label + ')');
-        });
-        // daterangepicker end
-
-        // Qr Kod Göster start
-        $('body').on('click', '.showqr', function() {
+        // Düzenlenecek user ı getirme start
+        $('body').on('click', '.editUser', function() {
             var formData = {};
             formData['id'] = $(this).data("id");
             $.ajax({
-                url: '/admin/reservation/showQr',
-                type: 'POST',
-                data: formData,
-                success: function(data) {
-                    if (data.status) {
-                        $('#qrModal').modal('show');
-                        $('.qr').html(data.html);
-                        console.log(data.html);
-                    } else {
-                        console.log(data);
-                        // toastr.warning(lang['voucher_not_found']);
-                    }
-                }
-            });
-
-        });
-        // Qr Kod Göster end
-
-
-        // Düzenlenecek rezervasyonu getirme start
-        $('body').on('click', '.editReservation', function() {
-            var formData = {};
-            formData['id'] = $(this).data("id");
-            $.ajax({
-                url: '/admin/reservation/get',
+                url: '/admin/user/get',
                 type: 'POST',
                 data: formData,
                 success: function(data) {
                     if (data.status) {
                         $('#editModal').modal('show');
-                        $('#editModal input[name="Amount"]').val(data.reservation.Amount);
-                        $('#editModal input[name="FirstName"]').val(data.reservation.FirstName);
-                        $('#editModal input[name="LastName"]').val(data.reservation.LastName);
-                        $('#editModal input[name="Pax"]').val(data.reservation.Pax);
-                        $('#editModal textarea[name="ReservationNote"]').val(data.reservation
-                            .ReservationNote);
-                        $('#editModal input[name="TableNumber"]').val(data.reservation.TableNumber);
-                        $('#editModal input[name="CheckInDate"]').val(data.reservation.CheckInDate);
-                        $('#editModal input[name="id"]').val(data.reservation.id);
+                        $('#editModal input[name="name"]').val(data.user.name);
+                        $('#editModal input[name="email"]').val(data.reservation.email);
                     } else {
                         console.log(data);
-                        // toastr.warning(lang['voucher_not_found']);
                     }
                 }
             });
 
         });
-        // Düzenlenecek rezervasyonu getirme end
+        // Düzenlenecek user ı getirme end
 
         // düzenlemleri kaydet start
         $('body').on('click', '.saveEdited', function() {
